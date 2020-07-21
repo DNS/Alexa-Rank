a76791ba97c362ff906df15d0689d33ebd16ad78
dns <sirait10@gmail.com>
2020-07-22 05:13:01 +0700

    -

--- example.py
+++ example.py
@@ -2,19 +2,42 @@
 
 from alexa import alexa_rank
 
+'''
 alexa_rank('udemy.com')
-alexa_rank('lynda.com')
 alexa_rank('khanacademy.org')
-alexa_rank('codecademy.com')
 alexa_rank('coursera.org')
-alexa_rank('sololearn.com')
-alexa_rank('skillshare.com')
 alexa_rank('edx.org')
+alexa_rank('codecademy.com')
+alexa_rank('skillshare.com')
 alexa_rank('udacity.com')
-alexa_rank('tokopedia.com')
+alexa_rank('lynda.com')
+alexa_rank('sololearn.com')
+'''
 
-alexa_rank('random27374e2378web.com')
+#alexa_rank('random27374e2378web.com')
+'''
+alexa_rank('ovagames.com')
+alexa_rank('igg-games.com')
+alexa_rank('rarbg.to')
+alexa_rank('thepiratebay.org')
+alexa_rank('1337x.to')
+'''
+alexa_rank('qasir.id')
+alexa_rank('shopee.com')
+alexa_rank('lazada.com')
+alexa_rank('bukalapak.com')
 
+#alexa_rank('.com')
 
-
+'''
+udemy.com United States rank: 130
+khanacademy.org United States rank: 163
+coursera.org United States rank: 424
+codecademy.com United States rank: 1,160
+skillshare.com United States rank: 1,248
+edx.org United States rank: 1,397
+lynda.com United States rank: 1,812
+udacity.com United States rank: 2,911
+sololearn.com United States rank: 15,022
+'''
 
