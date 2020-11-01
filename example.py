dc96164af8a6247ee39b7e86bffe654db445d669
dns <sirait10@gmail.com>
2020-11-01 16:59:22 +0700

    -

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 from alexa import alexa_rank
 
-'''
+
 alexa_rank('udemy.com')
 alexa_rank('khanacademy.org')
 alexa_rank('coursera.org')
@@ -12,10 +12,7 @@ alexa_rank('skillshare.com')
 alexa_rank('udacity.com')
 alexa_rank('lynda.com')
 alexa_rank('sololearn.com')
-'''
 
-#alexa_rank('random27374e2378web.com')
-'''
 alexa_rank('ovagames.com')
 alexa_rank('igg-games.com')
 alexa_rank('rarbg.to')
@@ -26,22 +23,10 @@ alexa_rank('qasir.id')
 alexa_rank('shopee.com')
 alexa_rank('lazada.com')
 alexa_rank('bukalapak.com')
-'''
+
 alexa_rank('tinder.com')
 alexa_rank('okcupid.com')
 alexa_rank('bumble.com')
 
-#alexa_rank('.com')
 
-'''
-udemy.com United States rank: 130
-khanacademy.org United States rank: 163
-coursera.org United States rank: 424
-codecademy.com United States rank: 1,160
-skillshare.com United States rank: 1,248
-edx.org United States rank: 1,397
-lynda.com United States rank: 1,812
-udacity.com United States rank: 2,911
-sololearn.com United States rank: 15,022
-'''
 
