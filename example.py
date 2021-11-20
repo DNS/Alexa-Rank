21e8326842f055007341c1b44044c19495867aeb
dns <sirait10@gmail.com>
2021-11-20 10:24:16 +0700

    R: alexa OLD.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 from alexa import alexa_rank
 
-
+'''
 alexa_rank('udemy.com')
 alexa_rank('khanacademy.org')
 alexa_rank('coursera.org')
@@ -18,7 +18,31 @@ alexa_rank('igg-games.com')
 alexa_rank('rarbg.to')
 alexa_rank('thepiratebay.org')
 alexa_rank('1337x.to')
+'''
+######
 
+alexa_rank('tokopedia.com')
+alexa_rank('shopee.com')
+alexa_rank('shopee.co.id')
+alexa_rank('bukalapak.com')
+alexa_rank('lazada.com')
+alexa_rank('lazada.co.id')
+alexa_rank('olx.com')
+alexa_rank('olx.co.id')
+
+#alexa_rank('finance.yahoo.com')
+#alexa_rank('markets.ft.com')
+#alexa_rank('bloomberg.com')
+#alexa_rank('quandl.com')
+
+'''
+tokopedia.com Indonesia rank: 9
+shopee.co.id Indonesia rank: 22
+lazada.co.id Indonesia rank: 75
+olx.co.id Indonesia rank: 91
+
+
+'''
 
 
 
